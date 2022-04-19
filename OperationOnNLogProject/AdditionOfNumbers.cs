c0bc026f899a43481c6068ff5528ce98318d0d5e
vinaykumar7081 <vinaykumarvinay165@gmail.com>
2022-04-19 13:41:48 +0530

    [VINAY] Add: Displaying Addition Of Addition Of Two Number

---
+++ OperationOnNLogProject/AdditionOfNumbers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperationOnNLogProject
+{
+    public class AdditionOfNumbers
+    {
+        private readonly NLog nLog = new NLog();
+        public int Add(int x, int y)
+        {
+            if (x == 0 || y == 0)
+            {
+                nLog.LogDebug("Debug Successfull : Sum()");
+                nLog.LogError("Expecting not null values");
+                nLog.LogWarn("FirstNumber or secondNumber should not equal to 0");
+            }
+            int result = x + y;
+            nLog.LogDebug("Debug Successfull : Sum()");
+            nLog.LogInfo("Sum Method passed, Result" + result);
+            return result;
+        }
+    }
+}
---
+++ OperationOnNLogProject/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using OperationOnNLogProject;
+class Program
+{
+    public static void Main(String[] args)
+    {
+        AdditionOfNumbers add = new AdditionOfNumbers();
+        Console.WriteLine("Enter two Number");
+        int x = Convert.ToInt32(Console.ReadLine());
+        int y = Convert.ToInt32(Console.ReadLine());
+        int z = add.Add(x, y);
+        Console.WriteLine("Result is :-->" + z);
+    }
+}
